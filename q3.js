00dbdeb3ccee8888ebe4bd7b6785580a70f5f222
André Alfonso Peixoto <alfonso.peixoto@gmail.com>
2021-12-23 03:40:47 -0500

    Atualização geral
    1. Eliminação de linhas sem funcionalidade implementada.
    2. Mudança de determinados comentários de "alert" para "console.log" não comentado.

--- q3.js
+++ q3.js
@@ -1,36 +1,153 @@
-//Trabalho Final - Atividade 02
-//Disciplina: Fundamentos Teóricos da Computação
-//Docente: André Luiz Nasserala
-//Discente: Eduardo Pinheiro Júnior
-//Resolução da primeira questão do trabalho final
+//Programa de Pós-Graduação em Ciência da Computação (PPGCC)
+//Universidade Federal do Acre (Ufac)
+//Disciplina: Fundamentos Teóricos da Computação (FTC)
+//Docente: André Luiz Nasserala Pires
+//Estudante: André Alfonso Peixoto
+//Trabalho final
 
-var dict = {};
-var linhas = 0;
-var contador_de_erros = 0;
-controle_erros = 0;
+function questao1(){
+	var E = ['ap', 'fp'];
+	var alfabeto = ['t', '(', ')', '+', '-'];
+	var lambda = [];
+	var i = E[0];
+	var F = E[1];
+	var estado_atual = i;
+	var expr = prompt("Informe uma expressão: ");
+	expr = expr.split('');
+
+	for (var j = 0; j<=expr.length; j++){
+		if(estado_atual === i && expr[j] === "("){
+					lambda[0] = 'x';
+					console.log("Entrada de ( e valor de lambda: " + lambda[j]);
+		}
+		if(estado_atual === F && expr[j] === ")"){
+					estado_atual = E[1];
+					lambda.pop();
+					console.log("Entrada de )");
+		}
+
+		if(estado_atual === F && expr[j] === "+" || expr[j] == "-"){
+					estado_atual = E[0];
+					console.log("Entrada de operacao");
+		}
+
+		if(estado_atual === i && !isNaN(expr[j])){
+					estado_atual = E[1];
+					console.log("Entrada de numero");
+		}
+	}
+	
+	console.log(expr.length);
+	console.log(j);
+	console.log(F);
+	console.log(estado_atual);
+	console.log(lambda);
+	
+	if(estado_atual == F && lambda){
+				console.log("Expressão aceita");
+				alert("Expressão aceita");
+	}else{
+				console.log("Expressão não aceita");
+				alert("Expressão não aceita");
+	}
+	
+}
+
+function questao2(){
+	var E = ['i', 'd'];
+	var alfabeto = ['0', '1'];
+	var lambda = [];
+	var i = E[0];
+	var F = E[0];
+	var estado_atual = i;
+	var expr = prompt("Informe uma expressão: ");
+	expr = expr.split('');
+
+	for(var j=0; j<=expr.length; j++){
+		if(estado_atual == i && expr[j] == "0"){
+			estado_atual = E[1];
+			if (lambda){
+				lambda = ['F'];
+				console.log("Recebe 0 e lambda: " + lambda[j]);
+				console.log(estado_atual);
+			}else{
+				lambda.push('F');
+				console.log(lambda);
+			}
+			console.log(lambda[k]);
+		}
+
+		else if(estado_atual == E[0] && expr[j] == '1'){
+			estado_atual = E[1];
+			if (lambda){
+				lambda = ['U'];
+			}else{
+				lambda.push('U');
+			}
+			console.log(lambda[k]);
+		}
+
+		else if(estado_atual == E[1] && expr[j] == '0'){
+			estado_atual = E[0];
+			
+			console.log("Recebe 0 de novo e lambda: " + lambda[j]);
+			console.log(estado_atual);
+			
+			for(var k = 0; k<=2; k++){
+				if(lambda == 'U'){
+					lambda.pop('U');
+				} if(lambda == 'F'){
+					lambda.pop('F');
+
+				}
+				console.log(lambda[k]);
+			}
+		}
+
+
+			else if(estado_atual == E[1] && expr[j] == '1'){
+			estado_atual = E[0];
+			
+			for(var k = 0; k<=2; k++){
+				if(lambda == 'F'){
+					lambda.pop('F');
+				} if(lambda == 'U'){
+					lambda.pop('U');
+
+				}
+				console.log(lambda[k]);
+			}
+		}
+
+	}
+	
+	console.log(estado_atual);
+	
+	if(estado_atual == F && lambda){
+			alert("A palavra "+expr+" é válida.");
+		}else{
+			alert("A palavra "+expr+" é inválida.");
+	}
+}
 
 /*
-function adiciona_erro(str) {
-	contador_de_erros;
-	contador_de_erros += 1;
-	controle_erros.append(str);
-}
+E = {q0, q1, q2}
+Σ = {t, +, -}
+t pertence a N
+δ:
+q0 <- t -> q1
+q0 <- + | - -> q2
+q1 <- t -> q2
+q1 <- - | + -> q0
+q2 <- - | + | t -> q2
+i = q0
+F = q0
+Γ = {x} 
 */
 
-alert('-h [--hel] - lista as funcoes da ;\n' +
-        '-m [--mostra] - printa uma mensagem qualquer. Ex: -m "Hello Word" or --mostra "Hello Word";\n' +
-        '-l [--limpa] - limpar a saída do console \n' + 
-        '-i [--int] - declara variáveis inteiras \n' + 
-        '\tEx.: -i x=10, y=20 or --int x=10\n' + 
-        '-op [--operacao] - executa operacao atitimática simples entre dois números\n' +
-        '\tEx.-op 1-2 or -op 1 + 2\n' +
-        '-a [--aceitacao] - verifica aceitação da expressaão\n' +
-        '\tEx.-a x-y or -a x + y\n' +
-        '-x [--exit] - sair do console\n'
-		)
-
-function verifica_expressao(exp){
-	var exp = prompt("Informe uma expressão: ")
+function verifica_expressao(){
+		var contador_erros;
+	var exp = prompt("Informe uma expressão")
 	exp = exp.split('');
 	E = ['q0', 'q1', 'q2'];
     i = E[0];
@@ -67,22 +184,17 @@ function verifica_expressao(exp){
 }
 
 function mostra(retorno) {
+	var contador_erros;
     retorno = retorno.split('',1);
     if ('"' in retorno) {
         retorno = retorno.replace('"', '');
-        //print('<%d>..: %s ' % (add_linhas(), retorno));
-        contador_de_erros += 1;
-		//add_erro('[Erro linha %d]' % linha);
+        contador_erros += 1;
     }
 	else if (retorno in dict) {
-        //print('<%d>..: %s' % (add_linhas(), dict[retorno]));
-        contador_de_erros += 1;
-		//add_erro('[Erro linha %d]' % linha);
+        contador_erros += 1;
 	}
 	else if (not retorno in dict) {
-        //print('<%d>..:[Erro na linha %d] %s não definido' % (add_linhas(), linha, retorno));
-        contador_de_erros += 1;
-		//add_erro('[Erro linha %d]' % linha);
+        contador_erros += 1;
 	}
 }
 
@@ -90,252 +202,44 @@ function limpa() {
     document.body.innerHTML = "";
 }
 
-function inteiro(opcao) {
-	opcao = opcao.split('');
-	if opcao instanceof String {
-		opcao = opcao[1].replace(' ', '');
+function inteiro() {
+	var contador_erros;
+	var declara_inteiro = prompt("Informe uma operação, como no seguinte exemplo: int x,y")
+	if declara_inteiro instanceof String {
+		declara_inteiro = declara_inteiro[1].replace(' ', '');
 	}
-	if (opcao == ",") {
-		opcao = opcao.split(',');
+	if (declara_inteiro == ",") {
+		declara_inteiro = declara_inteiro.split(',');
 	}
-	if (',' in opcao) {
-		opcao = opcao.split(',');
+	if (',' in declara_inteiro) {
+		declara_inteiro = declara_inteiro.split(',');
 	}
-	for (var j = 0; j <= opcao; j++) {
-		if (opcao != "=") {
-			//print('<%d>..:[Erro na linha %d] Valor inteiro não informado' % (add_linhas(), linha))
-            //add_erro('[Erro linha %d]' % linha)
-            contador_de_erros += 1;
-			return false;
-        }
-		temp = j.split('=');
-		if (!isNaN(temp[1])) {
-			//print('<%d>..:[Erro na linha %d] Valor inteiro não informado' % (add_linhas(), linha))
-            //add_erro('[Erro linha %d]' % linha)
-            contador_de_erros += 1;
-			return false;
-		}
-		//armazena_memoria(temp[0], int(temp[1]));
-        return true;
+	if (declara_inteiro != "=") {
+		contador_erros += 1;
+		return false;
+	}
+	if else (!isNaN(declara_inteiro[1])) {
+		contador_erros += 1;
+	}
     else {
-        if (opcao != '=') {
-            // print('<%d>..:[Erro na linha %d] O operador "=" não foi informado' % (add_linhas(), linha))
-            // add_erro('[Erro linha %d]' % linha)
-            contador_de_erros += 1;
-			return false;
+        if (declara_inteiro != '=') {
+            contador_erros += 1;
 		}
-        temp = opcao.split('=');
-        if (!isNaN(temp[1])) {
-            //print('<%d>..:[Erro na linha %d] Valor inteiro não informado' % (add_linhas(), linha))
-            //add_erro('[Erro linha %d]' % linha)
+        if (!isNaN(declara_inteiro[1])) {
             controle_erros += 1;
-			return false;
-		}
-        //armazena_memoria(temp[0], int(temp[1]));
-        return true;
 		}
 	}
 }
 
 function operacao() {
-	opcao = opcao.split('', 1);
-	if ('+' in opcao) {
+	var contador_erros;
+	var operacao = prompt("Informe uma operação, como no seguinte exemplo: operação(x,y,+)")
+	if ('+' in operacao) {
 		operacao = opcao.split('+');
-		if (!(operacao[0] in dict))
-			//print('<%d>..:[Erro na linha %d] Valor não encontrado' % (add_linhas(), linha))
-            //add_erro('[Erro linha %d]' % linha)
-            
-			return false;
-		}
-		//operacao = var[operacao[0]] + var[operacao[1]]
-        //print('<%d>..: %s' % (add_linhas() ,operacao))
-        return true;
-    if '-' in opcao {
+		operacao = var[operacao[0]] + var[operacao[1]]
+    if else ('-' in operacao) {
         operacao = opcao.split('-')
-        if not operacao[0] in var or not operacao[1] in var {
-            print('<%d>..:[Erro na linha %d] Valor não encontrado' % (add_linhas(), linha))
-            add_erro('[Erro linha %d]' % linha);
-            return false;
-		}
         operacao = var[operacao[0]] - var[operacao[1]];
-        //print('<%d>..: %s' % (add_linhas() ,operacao))
-        return true;
-	}
-}
-
-opcao = '';
-saida = 1;
-
-function q3() {
-	alert('-h ou --help para mais informações');
-	while(saida!=0) {
-		opcao = input('<%d>..: '%add_linhas());
-
-		if '-h' in opcao or '--help' in opcao {
-			help();
-		}
-		
-		else if '-m' in opcao or '--mostra' in opcao {
-			mostra(opcao);
-		}
-		
-		else if '-l' in opcao or '--limpa' in opcao {
-			limpa();
-		}
-
-		else if '-i' in opcao or  '--int' in opcao {
-			inteiro(opcao);
-		}
-
-		else if '-op' in opcao or '--operacao' in opcao {
-			operacao(opcao);
-		}
-			
-		else if '-x' in opcao or '--exit' in opcao {
-			saida=0;
-		}
-		
-		else if '-a' in opcao or '--aceitacao' in opcao {
-			verifica_expressao(opcao);
-		}
-		
-		else {
-			//print('<%d>..: [Erro na linha %d] Escolha uma opção válida.' % (add_linhas(),linhas))
-			//add_erro('[Erro linha %d]' % linhas)
-			contador_de_erros += 1;
-			help();
-	}
-}
-
-/*
-function q01(){
-	var E = ['ap', 'fp'];
-	var alfabeto = ['t', '(', ')', '+', '-'];
-	var lambda = [];
-	var i = E[0];
-	var F = E[1];
-	var estado_atual = i;
-	var expr = prompt("Informe uma expressão: ");
-	//alert(expr);
-	expr = expr.split('');
-	//alert(expr);
-	//alert("Olá");
-	//alert(expr.length);
-
-	for (var j = 0; j<=expr.length; j++){
-		if(estado_atual === i && expr[j] === "("){
-					lambda[0] = 'x';
-					//alert("Entrada de ( e valor de lambda: " + lambda[j]);
-		}
-		if(estado_atual === F && expr[j] === ")"){
-					estado_atual = E[1];
-					lambda.pop();
-					//alert("Entrada de )");
-		}
-
-		if(estado_atual === F && expr[j] === "+" || expr[j] == "-"){
-					estado_atual = E[0];
-					//alert("Entrada de operacao");
-		}
-
-		if(estado_atual === i && !isNaN(expr[j])){
-					estado_atual = E[1];
-					//alert("Entrada de numero");
-		}
-	}
-	
-	//alert(expr.length);
-	//alert(j);
-	//alert(F);
-	//alert(estado_atual);
-	//alert(lambda);
-	
-	if(estado_atual == F && lambda){
-				console.log("Expressão aceita");
-				alert("Expressão aceita");
 	}else{
-				console.log("Expressão não aceita");
-				alert("Expressão não aceita");
-	}
-	
+		alert(
 }
-//Resolução da segunda questão do trabalho final
-
-function q02(){
-	var E = ['i', 'd'];
-	var alfabeto = ['0', '1'];
-	var lambda = [];
-	var i = E[0];
-	var F = E[0];
-	var estado_atual = i;
-	var expr = prompt("Informe uma expressão: ");
-	expr = expr.split('');
-
-	for(var j=0; j<=expr.length; j++){
-		if(estado_atual == i && expr[j] == "0"){
-			estado_atual = E[1];
-			if (lambda){
-				lambda = ['F'];
-				//alert("Recebe 0 e lambda: " + lambda[j]);
-				//alert(estado_atual);
-			}else{
-				lambda.push('F');
-				console.log(lambda);
-			}
-			console.log(lambda[k]);
-		}
-
-		else if(estado_atual == E[0] && expr[j] == '1'){
-			estado_atual = E[1];
-			if (lambda){
-				lambda = ['U'];
-			}else{
-				lambda.push('U');
-			}
-			console.log(lambda[k]);
-		}
-
-		else if(estado_atual == E[1] && expr[j] == '0'){
-			estado_atual = E[0];
-			
-			//alert("Recebe 0 de novo e lambda: " + lambda[j]);
-			//alert(estado_atual);
-			
-			for(var k = 0; k<=2; k++){
-				if(lambda == 'U'){
-					lambda.pop('U');
-				} if(lambda == 'F'){
-					lambda.pop('F');
-
-				}
-				console.log(lambda[k]);
-			}
-		}
-
-
-			else if(estado_atual == E[1] && expr[j] == '1'){
-			estado_atual = E[0];
-			
-			for(var k = 0; k<=2; k++){
-				if(lambda == 'F'){
-					lambda.pop('F');
-				} if(lambda == 'U'){
-					lambda.pop('U');
-
-				}
-				console.log(lambda[k]);
-			}
-		}
-
-	}
-	
-	//alert(estado_atual);
-	
-	if(estado_atual == F && lambda){
-			console.log("A palavra "+expr+" é reconhecida!!");
-		}else{
-			console.log("A palavra "+expr+" não é reconhecida!");
-	}
-}
-
-*/
